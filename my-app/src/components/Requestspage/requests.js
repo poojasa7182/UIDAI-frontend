615e8c1c0fbce9a1cdfc62eff47a52de44c1cb21
Paritosh_Kabra <paritosh_k@cs.iitr.ac.in>
2021-10-31 22:39:06 +0530

    final testing before hackathon

--- my-app/src/components/Requestspage/requests.js
+++ my-app/src/components/Requestspage/requests.js
@@ -14,6 +14,7 @@ import {
   DialogContent,
   DialogContentText,
 } from "@mui/material";
+import axios from "axios";
 const useStyles = makeStyles((theme) => ({
   containerReq: {
     padding: "2%",
@@ -46,19 +47,58 @@ const useStyles = makeStyles((theme) => ({
 
 function Requests(props) {
   const classes = useStyles();
-
-  const clients = [
-    {
-      name: "Pooja Allampallewar",
-      aadharNo: "567572542338",
-    },
-    {
-      name: "dfede",
-      aadharNo: "dsds",
-    },
-  ];
+  const [open, setOpen] = React.useState(false);
+  const [requests, setRequests] = React.useState(null);
+  const [currRequest, setCurrRequest] = React.useState(null);
+  React.useEffect(() => {
+    getRequests();
+  }, []);
   const isActive = useMediaQuery("(max-width : 700px)");
-
+  const handleClose = () => {
+    setOpen(false);
+    setCurrRequest(null);
+  };
+  const getRequests = () => {
+    axios
+      .get(
+        "http://127.0.0.1:8000/uidai/introducer_request/" +
+          sessionStorage.getItem("user") +
+          "/",
+        {
+          headers: {
+            Authorization: `Token ${sessionStorage.getItem("token")}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log("request_sent: ", res.data);
+        setRequests(res.data["request_introducer"]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const declineRequest = () => {
+    console.log(currRequest);
+    const data = {
+      introducer: sessionStorage.getItem("user"),
+      client: currRequest.client_id,
+      status: "declined",
+    };
+    axios
+      .put("http://127.0.0.1:8000/uidai/sent/" + currRequest.id + "/", data, {
+        headers: {
+          Authorization: `Token ${sessionStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => {
+        console.log("Request declined successfully!!", res.data);
+        handleClose();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <Container
       className={classes.containerReq}
@@ -76,148 +116,162 @@ function Requests(props) {
         Pending requests
       </Typography>
       <br></br>
-      {clients.map(function (client) {
-        return (
-          <Container
-            className={classes.inContainer}
-            sx={{ flexDirection: isActive ? "column" : "row" }}
-          >
-            <Box
-              className={classes.box1}
-              sx={{ width: isActive ? "100%" : "50%" }}
+      {requests !== null &&
+        requests.map(function (request, index) {
+          return (
+            <Container
+              className={classes.inContainer}
+              sx={{ flexDirection: isActive ? "column" : "row" }}
+              key={index.toString()}
             >
-              <Typography
-                variant="p"
-                sx={{
-                  fontSize: "1.5em",
-                  fontWeight: "bold",
-                  marginBottom: "2%",
-                }}
+              <Box
+                className={classes.box1}
+                sx={{ width: isActive ? "100%" : "50%" }}
               >
-                Client Details
-              </Typography>
-              <div>
                 <Typography
                   variant="p"
                   sx={{
-                    fontSize: "1.4em",
+                    fontSize: "1.5em",
                     fontWeight: "bold",
+                    marginBottom: "2%",
                   }}
                 >
-                  {client.name}
+                  Client Details
+                </Typography>
+                <div>
+                  <Typography
+                    variant="p"
+                    sx={{
+                      fontSize: "1.2em",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {request.client_name}&nbsp;
+                  </Typography>
+                  <Typography
+                    variant="p"
+                    sx={{
+                      fontSize: "1.2em",
+                    }}
+                  >
+                    {isActive ? <br></br> : "|  "}
+                    Aadhar:&nbsp;{request.client_aadhar}
+                  </Typography>
+                </div>
+                {isActive ? "" : <br></br>}
+              </Box>
+              {isActive ? (
+                <Divider></Divider>
+              ) : (
+                <Divider orientation="vertical" flexItem />
+              )}
+
+              <Box
+                className={classes.box2}
+                sx={{ width: isActive ? "100%" : "50%" }}
+              >
+                <Typography
+                  variant="p"
+                  sx={{
+                    fontSize: "1.5em",
+                    fontWeight: "bold",
+                    marginBottom: "2%",
+                  }}
+                >
+                  Status
                 </Typography>
                 <Typography
                   variant="p"
                   sx={{
                     fontSize: "1.2em",
+                    mb: "1em",
                   }}
                 >
-                  {isActive ? <br></br> : "|  "}
-                  Aadhar:{client.aadharNo}
+                  Waiting for your response
                 </Typography>
-              </div>
-              {isActive ? "" : <br></br>}
-            </Box>
-            {isActive ? (
-              <Divider></Divider>
-            ) : (
-              <Divider orientation="vertical" flexItem />
-            )}
+                <br />
+                <br />
 
-            <Box
-              className={classes.box2}
-              sx={{ width: isActive ? "100%" : "50%" }}
-            >
-              <Typography
-                variant="p"
-                sx={{
-                  fontSize: "1.5em",
-                  fontWeight: "bold",
-                  marginBottom: "2%",
-                }}
+                <div className={classes.buttons}>
+                  <Button
+                    variant="outlined"
+                    color="error"
+                    style={{
+                      maxWidth: "75%",
+                      padding: "1.5% 3%",
+                      color: "red",
+                    }}
+                    type="submit"
+                    sx={{
+                      borderRadius: 28,
+                      alignSelf: "center",
+                      justifySelf: "center",
+                      textAlign: "center",
+                    }}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setOpen(true);
+                      setCurrRequest(request);
+                    }}
+                  >
+                    Decline request
+                  </Button>
+                  {isActive ? <div>&nbsp;</div> : ""}
+                  <Button
+                    variant="outlined"
+                    style={{ maxWidth: "75%", padding: "1.5% 3%" }}
+                    type="submit"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      props.history.push(`/reqs/${request.id}`);
+                    }}
+                    sx={{
+                      borderRadius: 28,
+                      alignSelf: "center",
+                      justifySelf: "center",
+                      textAlign: "center",
+                    }}
+                  >
+                    Respond to request
+                  </Button>
+                </div>
+              </Box>
+              {isActive ? <br></br> : ""}
+              <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
               >
-                Status
-              </Typography>
-              <Typography
-                variant="p"
-                sx={{
-                  fontSize: "1.2em",
-                }}
-              >
-                Waiting for your response
-              </Typography>
-              <br></br>
-              <br></br>
-              <div className={classes.buttons}>
-                <Button
-                  variant="outlined"
-                  color="error"
-                  style={{ maxWidth: "75%", padding: "1.5% 3%", color: "red" }}
-                  type="submit"
-                  sx={{
-                    borderRadius: 28,
-                    alignSelf: "center",
-                    justifySelf: "center",
-                    textAlign: "center",
-                  }}
-                  onClick={() => {}}
-                >
-                  Cancel request
-                </Button>
-                {isActive ? <div>&nbsp;</div> : ""}
-                <Button
-                  variant="outlined"
-                  style={{ maxWidth: "75%", padding: "1.5% 3%" }}
-                  type="submit"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    props.history.push("/req");
-                  }}
-                  sx={{
-                    borderRadius: 28,
-                    alignSelf: "center",
-                    justifySelf: "center",
-                    textAlign: "center",
-                  }}
-                >
-                  Respond to request
-                </Button>
-              </div>
-            </Box>
-            {isActive ? <br></br> : ""}
-            {/* <Dialog
-              open={open}
-              onClose={handleClose}
-              aria-labelledby="alert-dialog-title"
-              aria-describedby="alert-dialog-description"
-            >
-              <DialogTitle id="alert-dialog-title">Delete Request</DialogTitle>
-              <DialogContent>
-                <DialogContentText id="alert-dialog-description">
-                  Are you sure cancel the request?
-                </DialogContentText>
-              </DialogContent>
-              <DialogActions>
-                <Button
-                  onClick={handleClose}
-                  variant="contained"
-                  color="primary"
-                >
-                  No
-                </Button>
-                <Button
-                  onClick={deleteRequest}
-                  autoFocus
-                  variant="contained"
-                  color="secondary"
-                >
-                  Yes, delete request
-                </Button>
-              </DialogActions>
-            </Dialog> */}
-          </Container>
-        );
-      })}
+                <DialogTitle id="alert-dialog-title">
+                  Delete Request
+                </DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="alert-dialog-description">
+                    Are you sure decline the request?
+                  </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button
+                    onClick={handleClose}
+                    variant="outlined"
+                    color="primary"
+                  >
+                    No
+                  </Button>
+                  <Button
+                    onClick={declineRequest}
+                    autoFocus
+                    variant="outlined"
+                    color="secondary"
+                  >
+                    Yes, decline request
+                  </Button>
+                </DialogActions>
+              </Dialog>
+            </Container>
+          );
+        })}
     </Container>
   );
 }
