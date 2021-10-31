6e0fb837c4e66ebba043f9aac9f7ac46574c00f7
pooja allampallewar <poojasa718@gmail.com>
2021-10-31 17:37:09 +0530

    form conflicts

--- my-app/src/components/FormPage/form.js
+++ my-app/src/components/FormPage/form.js
@@ -6,15 +6,11 @@ import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import { makeStyles } from "@mui/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
-<<<<<<< HEAD
-import { Divider } from "@mui/material";
-=======
 import { Divider } from '@mui/material';
 import Snackbar from '@mui/material/Snackbar';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import axios from "axios";
->>>>>>> 78f2e5732b340f1211cd1b48607902446d4df26e
 
 const useStyles = makeStyles((theme) => ({
     containerReq: {
@@ -23,8 +19,6 @@ const useStyles = makeStyles((theme) => ({
         flexDirection: "column",
         overflowY: "auto",
     },
-<<<<<<< HEAD
-=======
     containerReq2: {
         padding: '2%',
         display: 'flex !important',
@@ -33,44 +27,12 @@ const useStyles = makeStyles((theme) => ({
         overflowY: 'auto',
     },
 
->>>>>>> 78f2e5732b340f1211cd1b48607902446d4df26e
 }));
 
 
 
 function Requests(props) {
     const classes = useStyles();
-<<<<<<< HEAD
-    // const [address, setAddress] = React.useState('');
-    // @house": "34/1 Type- V Ground Floor",
-    //  "@pc": "110001",
-    const address = {
-        country: "India",
-        state: "Delhi",
-        dist: "New Delhi",
-        subDist: "New Delhi",
-        po: "New Delhi G.p",
-        pc: "110001",
-        vtc: "New Delhi G.P.O.",
-        landmark: "",
-        loc: "Kali Bari Marg, DIZ Area",
-        street: "BSNL Colony",
-        house: "34/1 Type- V Ground Floor",
-    };
-    const [loc, setLoc] = React.useState(address.loc);
-    const [street, setStreet] = React.useState(address.street);
-    const [house, setHouse] = React.useState(address.house);
-    const isActive = useMediaQuery("(max-width : 700px)");
-
-    return (
-        <Container
-            className={classes.containerReq}
-            sx={{ height: !isActive ? "90vh" : "auto" }}
-        >
-            <Typography
-                variant="p"
-                sx={{
-=======
     const [pass,setPass]= React.useState('');
     const [open, setOpen] = React.useState(false);
     const [address, setAddress] = React.useState('');
@@ -148,137 +110,10 @@ function Requests(props) {
             <Typography variant = "p"
             sx = {
                 {
->>>>>>> 78f2e5732b340f1211cd1b48607902446d4df26e
                     fontSize: "1.5em",
                     fontWeight: "bold",
                     className: classes.head,
                     textAlign: isActive ? "justify !important" : "center",
-<<<<<<< HEAD
-                }}
-            >
-                Address Form{" "}
-            </Typography>{" "}
-            <br />
-            <TextField
-                id="outlined-read-only-input"
-                label="House Number/Location"
-                defaultValue={address.house}
-                value={house}
-                sx={{ zIndex: "-1" }}
-                onChange={(e) => setHouse(e.target.value)}
-            />{" "}
-            <br />
-            <TextField
-                id="outlined-read-only-input"
-                label="Street"
-                sx={{ zIndex: "-1" }}
-                defaultValue={address.street}
-                value={street}
-                onChange={(e) => setStreet(e.target.value)}
-            />{" "}
-            <br />
-            <TextField
-                id="outlined-read-only-input"
-                label="Landmark"
-                sx={{ zIndex: "-1" }}
-                defaultValue={address.landmark}
-                value={loc}
-                onChange={(e) => setLoc(e.target.value)}
-
-            />
-            <br />
-            <TextField
-                id="outlined-read-only-input"
-                label="Location"
-                sx={{ zIndex: "-1" }}
-                defaultValue={address.loc}
-                InputProps={{
-                    readOnly: true,
-                }}
-            />{" "}
-            <br />
-            <TextField
-                id="outlined-read-only-input"
-                label="Village / Town / City"
-                defaultValue={address.vtc}
-                sx={{ zIndex: "-1" }}
-                InputProps={{
-                    readOnly: true,
-                }}
-            />{" "}
-            <br />
-            <TextField
-                id="outlined-read-only-input"
-                label="Post Office"
-                sx={{ zIndex: "-1" }}
-                defaultValue={address.po}
-                InputProps={{
-                    readOnly: true,
-                }}
-            />{" "}
-            <br />
-            <TextField
-                id="outlined-read-only-input"
-                label=" Sub District"
-                sx={{ zIndex: "-1" }}
-                defaultValue={address.subDist}
-                InputProps={{
-                    readOnly: true,
-                }}
-            />{" "}
-            <br />
-            <TextField
-                id="outlined-read-only-input"
-                label="District"
-                sx={{ zIndex: "-1" }}
-                defaultValue={address.dist}
-                InputProps={{
-                    readOnly: true,
-                }}
-            />{" "}
-            <br />
-            <TextField
-                id="outlined-read-only-input"
-                label="Pin Code"
-                sx={{ zIndex: "-1" }}
-                defaultValue={address.pc}
-                InputProps={{
-                    readOnly: true,
-                }}
-            />{" "}
-            <br />
-            <TextField
-                id="outlined-read-only-input"
-                label="State"
-                sx={{ zIndex: "-1" }}
-                defaultValue={address.state}
-                InputProps={{
-                    readOnly: true,
-                }}
-            />{" "}
-            <br />
-            <TextField
-                id="outlined-read-only-input"
-                label="Country"
-                sx={{ zIndex: "-1" }}
-                defaultValue={address.country}
-                InputProps={{
-                    readOnly: true,
-                }}
-            />{" "}
-            <br />
-            <Button
-                variant="contained"
-                color="success"
-                style={{ maxWidth: "75%", padding: "1.5% 3%" }}
-                type="submit"
-                sx={{ borderRadius: 28, alignSelf: "center" }}
-            >
-                Submit address details{" "}
-            </Button>{" "}
-        </Container>
-    );
-=======
                 }
             } >
             Address Form </Typography> <br/>
@@ -437,7 +272,6 @@ function Requests(props) {
 
 
   );
->>>>>>> 78f2e5732b340f1211cd1b48607902446d4df26e
 }
 
 export default Requests;
